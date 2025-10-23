56ad5ca30618ecae596dc9af5278a417fcee8d7e
PeterCoder <oselumense2020@gmail.com>
2025-10-23 17:28:30 +0100

    first code upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Components/Navbar.jsx'
+import './index.css'
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  )
+}
+export default App
