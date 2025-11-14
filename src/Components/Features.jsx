95153105484c7bd427d921d99fa34a026a477000
PeterCoder <oselumense2020@gmail.com>
2025-11-14 03:10:14 +0100

    Implementing Features section uploaded

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -1,16 +1,83 @@
 import { Canvas } from "@react-three/fiber"
 import StudioLights from "./three/StudioLights.jsx";
-import {features} from "../Constants/Index.js"; 
+import {features, featureSequence} from "../Constants/Index.js"; 
 import clsx from "clsx";
-import { Suspense, useRef } from "react";
+import { Suspense, useEffect, useRef } from "react";
 import { Html } from "@react-three/drei";
 import MacbookModel from "./models/Macbook.jsx";
 import { useMediaQuery } from "react-responsive";
+import useMacbookStore from "../Store/index.js";
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap';
+
 
 
 const ModelScroll = () => {
   const groupRef = useRef(null);
   const isMobile = useMediaQuery({ query: '(max-width: 1024px'});
+  const { setTexture } = useMacbookStore();
+
+  // Pre-load all feature videos during component mount 
+  useEffect(() => {
+    featureSequence.forEach((feature) => {
+      const v = document.createElement('video');
+
+      Object.assign(v, {
+        src: feature.videoPath,
+        muted: true,
+        playsInline: true,
+        preload: 'auto',
+        crossOrigin: 'anonymous',
+      });
+
+      v.load();
+    })
+  }, []);
+
+  useGSAP(() => {
+    // 3D MODEL ROTATION ANIMATION
+    const modelTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#f-canvas',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1,
+        pin: true,
+      }
+    });
+
+    // SYNC THE FEATURE CONTENT
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#f-canvas',
+        start: 'top center',
+        end: 'bottom top',
+        scrub: 1,
+      }
+    });
+
+    // 3D SPIN
+    if(groupRef.current) {
+      modelTimeline.to(groupRef.current.rotation, { y: Math.PI * 2, ease: 'power1.inOut'});
+    };
+
+    // Content & Texture Sync
+    timeline
+        .call(() => setTexture('/videos/feature-1.mp4'))
+        .to('.box1', { opacity: 1, y: 0, delay: 1 })
+
+        .call(() => setTexture('/videos/feature-2.mp4'))
+        .to('.box2', { opacity: 1, y: 0 })
+
+        .call(() => setTexture('/videos/feature-3.mp4'))
+        .to('.box3', { opacity: 1, y: 0 })
+
+        .call(() => setTexture('/videos/feature-4.mp4'))
+        .to('.box4', { opacity: 1, y: 0 })
+
+        .call(() => setTexture('/videos/feature-5.mp4'))
+        .to('.box5', { opacity: 1, y: 0 })
+  }, []);
 
   return (
     <group ref={groupRef}>
@@ -35,7 +102,13 @@ const Features = () => {
 
       <div className="absolute inset-0">
         {features.map((feature, index) => (
-          <div className={clsx('box',`box${index + 1}`, feature.styles)}>{feature.text}</div>
+          <div className={clsx('box',`box${index + 1}`, feature.styles)}>
+            <img src={feature.icon} alt={feature.highlight} />
+            <p>
+              <span className="text-white">{feature.highlight}</span>
+              {feature.text}
+            </p>
+          </div>
         ))}
       </div>
 
