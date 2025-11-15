8994ce87db93f1bda178d61230c234d3491d98fc
PeterCoder <oselumense2020@gmail.com>
2025-11-15 14:27:51 +0100

    Implement correction

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import { navLinks } from "../Constants/index.js";
+import {navLinks} from "../constants";
 
-const Navbar = () => {
-  return (
-    <header>
-        <nav>
-            <img src="/logo.svg" alt="Apple logo" />
+const NavBar = () => {
+    return (
+        <header>
+            <nav>
+                <img  src="/logo.svg" alt="Apple logo" />
 
-            <ul>
-                {navLinks.map(({ label }) => (
-                    <li key={label}>
-                        <a href={label}>{label}</a>
-                    </li> 
-                ))}
-            </ul>
+                <ul>
+                    {navLinks.map(({ label }) => (
+                        <li key={label}>
+                            <a href={label}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
 
-            <div className="flex-center gap-3">
-                <button>
-                    <img src="/search.svg" alt="Search" />
-                </button>
-                <button>
-                    <img src="/cart.svg" alt="Cart" />
-                </button>
-            </div>
-        </nav>
-    </header>
-  )
+                <div className="flex-center gap-3">
+                    <button>
+                        <img src="/search.svg" alt="Search" />
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="Cart" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
 }
-
-export default Navbar
+export default NavBar
