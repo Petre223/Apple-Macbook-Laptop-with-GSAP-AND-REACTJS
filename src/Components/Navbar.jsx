af006ee56333358a78dd3994c76eac597451455f
PeterCoder <oselumense2020@gmail.com>
2025-11-20 13:19:41 +0100

    code section

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { navLinks } from "../Constants/index.js";
+import { navLinks } from "../Constants/Index.js";
 
 const NavBar = () => {
     return (
--- src/Components/models/Macbook-16.jsx
+++ src/Components/models/Macbook-16.jsx
@@ -11,7 +11,7 @@ Title: macbook pro M3 16 inch 2024
 import React, { useEffect }  from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
 import useMacbookStore from '../../Store/index.js';
-import { noChangeParts } from '../../Constants/index.js';
+import { noChangeParts } from '../../Constants/Index.js';
 import { Color, SRGBColorSpace } from 'three';
 
 export default function MacbookModel16(props) {
