1911f3eabcddcd5cd467d01db4576b0ae0b3ecb8
PeterCoder <oselumense2020@gmail.com>
2025-11-20 12:43:27 +0100

    improvment of correction

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,29 +1,42 @@
-import { navLinks } from "../Constants";
+import { navLinks } from "../Constants/index.js";
 
 const NavBar = () => {
     return (
-        <header>
-            <nav>
-                <img  src="/logo.svg" alt="Apple logo" />
+        <header className="navbar">
+            <nav className="navbar-container" aria-label="Main navigation">
 
-                <ul>
-                    {navLinks.map(({ label }) => (
+                {/* Logo */}
+                <a href="/" className="navbar-logo">
+                    <img 
+                        src="/logo.svg" 
+                        alt="Apple logo" 
+                        className="logo-img"
+                    />
+                </a>
+
+                {/* Navigation Links */}
+                <ul className="navbar-links">
+                    {navLinks.map(({ label, path }) => (
                         <li key={label}>
-                            <a href={label}>{label}</a>
+                            <a href={path} className="nav-link">
+                                {label}
+                            </a>
                         </li>
                     ))}
                 </ul>
 
+                {/* Icons */}
                 <div className="flex-center gap-3">
-                    <button>
-                        <img src="/search.svg" alt="Search" />
+                    <button aria-label="Search" className="icon-button">
+                        <img src="/search.svg" alt="" aria-hidden="true" />
                     </button>
-                    <button>
-                        <img src="/cart.svg" alt="Cart" />
+                    <button aria-label="Cart" className="icon-button">
+                        <img src="/cart.svg" alt="" aria-hidden="true" />
                     </button>
                 </div>
             </nav>
         </header>
-    )
-}
-export default NavBar
+    );
+};
+
+export default NavBar;
